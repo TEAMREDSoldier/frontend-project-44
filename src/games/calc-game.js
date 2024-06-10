39ecd276e29cf50f3dafae1e9da0223dc01cf3fd
TEAMREDSdoldier <vysochinkirill6@gmail.com>
2024-06-10 15:55:44 +0500

    Фикс ошибок 2

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,23 +1,33 @@
 import startGame from '../index.js';
 import randoMumbers from '../randoMumbers.js';
 
-const getResultOfExpression = (firstValue, operator, secondValue) => {
-  return eval(`${firstValue} ${operator} ${secondValue}`);
-};
+const gameDescription = 'What is the result of the expression?';
 
 const operators = ['+', '-', '*'];
 
-const gameDescription = 'What is the result of the expression?';
+const operations = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+const getResultOfExpression = (firstValue, operator, secondValue) => {
+  const operation = operations[operator];
+  if (!operation) {
+    throw new Error(`Unsupported operator: ${operator}`);
+  }
+  return operation(firstValue, secondValue);
+};
 
 const generateRounds = () => {
   const firstNumber = randoMumbers(1, 25);
   const secondNumber = randoMumbers(1, 25);
   const operator = operators[randoMumbers(0, operators.length - 1)];
 
-  const question = `${firstNumber} ${operator} ${secondNumber}`;
-  const correctAnswer = getResultOfExpression(firstNumber, operator, secondNumber).toString();
-
-  return [question, correctAnswer];
+  return [
+    `${firstNumber} ${operator} ${secondNumber}`,
+    getResultOfExpression(firstNumber, operator, secondNumber).toString()
+  ];
 };
 
 export default () => {
